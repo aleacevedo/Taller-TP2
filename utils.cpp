66d14b697a343b6ca96e77207e3c851bb5283aff
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-17 17:55:43 -0300

    Modify utils

--- utils.cpp
+++ utils.cpp
@@ -4,18 +4,19 @@
 
 #include "utils.h"
 
+
 void print_byte_as_bits(char val) {
   for (int i = 7; 0 <= i; i--) {
     printf("%c", (val & (1 << i)) ? '1' : '0');
   }
 }
 
-void print_bits(char * ty, char * val, unsigned char * bytes, size_t num_bytes) {
-  printf("(%*s) %*s = [ ", 15, ty, 16, val);
+void print_bits(unsigned char * bytes, size_t num_bytes) {
+  printf("[ ");
   for (size_t i = 0; i < num_bytes; i++) {
     print_byte_as_bits(bytes[i]);
     printf(" ");
   }
-  printf("]\n");
+  printf("]");
 }
 
--- utils.h
+++ utils.h
@@ -5,7 +5,15 @@
 #ifndef RESOLUCION__UTILS_H_
 #define RESOLUCION__UTILS_H_
 
-void print_bits(char * ty, char * val, unsigned char * bytes, size_t num_bytes);
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <string>
+#include <arpa/inet.h>
+#include <errno.h>
+
+void print_byte_as_bits(char val);
+void print_bits(unsigned char * bytes, size_t num_bytes);
 
 
 #endif //RESOLUCION__UTILS_H_
