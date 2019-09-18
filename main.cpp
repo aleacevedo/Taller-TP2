19281021a5ee1ada832524c3edf9831b414bd18f
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-18 18:38:44 -0300

    Tests

--- main.cpp
+++ main.cpp
@@ -8,15 +8,13 @@ int main(int argc, char *argv[]) {
   fstream file_out("outPut", fstream::out);
   Compressor comp(file_in, 4);
   comp.one_run();
-  char *buffer = new char[comp.get_size_compressed()] ();
-  comp.get_compressed(buffer);
+  vector<char> &buffer = comp.get_compressed();
   //  file_out.write(comp.get_compressed(), comp.get_size_compressed());
   for (size_t i = 0; i < comp.get_size_compressed(); i++) {
     print_byte_as_bits(buffer[i]);
   }
   file_in.close();
   file_out.close();
-  delete[] buffer;
   return 0;
 }
 
