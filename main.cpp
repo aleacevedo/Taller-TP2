d6044a6b3c0a9f8381f3813bf5e963ede47179ae
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-22 10:03:00 -0300

    Add some test in main

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include <stdio.h>
+#include <fstream>
 #include "compressor.h"
+#include "generator.h"
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
-  std::ifstream file_in("alotMod", std::ifstream::binary);
-  std::ofstream file_out("outPut", std::ofstream::binary);
-  Compressor comp(file_in, 4);
-  while (file_in.good()) {
-    if (comp.one_run() == -1) {
-        file_in.close();
-        file_out.close();
-        return 0;
-    }
-    vector<char> &buffer = comp.get_compressed();
-    printf("New len: %zu \n", comp.get_new_len());
-    printf("Reference: %zu \n", comp.get_reference());
-    for (size_t i = 0; i < comp.get_size_packed(); i++) {
-      file_out.write(&(buffer[i]), 1);
-      print_byte_as_bits(buffer[i]);
-      printf(" ");
-    }
-    printf("\n");
-  }
+  std::ifstream file_in("alot", std::ifstream::binary);
+  std::ofstream file_out("outPut1", std::ofstream::binary);
+  Compressor *comp = new Compressor(file_in, 4);
+  std::vector<Compressor> comps;
+  comps.push_back(*comp);
+  Generator *generator = new Generator(comps, file_in);
+  
   file_in.close();
   file_out.close();
   return 0;
