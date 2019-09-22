e04b17c56e3e88a051f212fae47e9584f36e4cad
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-22 10:02:49 -0300

    Implement thread params class

---
+++ thread_params.cpp
@@ -0,0 +1,30 @@
+#include "thread_params.h"
+
+Params::Params(Compressor &compressor,
+              size_t index,
+              std::queue<std::string> *queue) : compressor(compressor),
+                                                index(index),
+                                                processed(0),
+                                                output(output) {
+
+                                                }
+
+Compressor& Params::get_compressor() {
+  return this->compressor;
+}
+
+size_t Params::get_index() {
+  return this->index;
+}
+
+size_t Params::get_processed() {
+  return this->processed;
+}
+
+std::queue<std::string>* Params::get_queue() {
+  return this->output;
+}
+
+void Params::inc_processed() {
+  this->processed++;
+}
---
+++ thread_params.h
@@ -0,0 +1,28 @@
+
+#ifndef _THREADS_PARAMS_H_
+#define _THREADS_PARAMS_H_
+#include <stdio.h>
+#include <queue>
+#include <string>
+#include "compressor.h"
+
+
+class Params{
+  Compressor &compressor;
+  size_t index;
+  size_t processed;
+  std::queue<std::string> *output;
+
+ public:
+  Params(Compressor &compressor,
+              size_t index,
+              std::queue<std::string> *output);
+  Compressor& get_compressor();
+  size_t get_index();
+  size_t get_processed();
+  std::queue<std::string>* get_queue();
+  void inc_processed();
+  ~Params();
+};
+
+#endif  //  _THREADS_PARAMS_H_
