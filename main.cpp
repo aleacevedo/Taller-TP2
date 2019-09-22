206d2b466791d8b6015e8912bdea793b19f2c4d6
alejoacevedo <ale.acevedo@live.com.ar>
2019-09-22 17:20:29 -0300

    User producer

--- main.cpp
+++ main.cpp
@@ -2,25 +2,25 @@
 #include <fstream>
 #include <thread>
 #include "compressor.h"
-#include "generator.h"
+#include "producer.h"
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
   std::ifstream file_in("alot", std::ifstream::binary);
-  std::ofstream file_out("outPut2", std::ofstream::binary);
+  std::ofstream file_out("outPut3", std::ofstream::binary);
   Compressor *comp = new Compressor(file_in, 4);
   std::vector<Compressor*> comps;
   std::vector<std::thread*> threads;
   comps.push_back(comp);
-  Generator generator(comps, file_in);
+  Producer producer(comps, file_in);
   for (size_t ind = 0; ind<comps.size(); ind++){
-    threads.push_back(new std::thread(std::ref(generator), ind));
+    threads.push_back(new std::thread(std::ref(producer), ind));
   }
   threads[0]->join();
-  while (!generator.get_outputs()[0]->empty()){
-    std::string out = generator.get_outputs()[0]->front();
-    file_out << out;
-    generator.get_outputs()[0]->pop();
+  while (!producer.get_outputs()[0]->empty()){
+    std::string out = producer.get_outputs()[0]->front();
+    file_out.write(out.c_str(), out.size());
+    producer.get_outputs()[0]->pop();
   }
   for (size_t ind = 0; ind<comps.size(); ind++){
     delete threads[ind];
