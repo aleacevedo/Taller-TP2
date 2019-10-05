c38fb7751c30bcb1fbc26dcf052b32f194b0b7ce
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-05 16:53:41 -0300

    Add iostream

--- compressor.cpp
+++ compressor.cpp
@@ -1,5 +1,5 @@
 #include "compressor.h"
-#include <stdio.h>
+#include <iostream>
 #include <string>
 #include <math.h>
 #include <arpa/inet.h>
--- consumer.cpp
+++ consumer.cpp
@@ -1,4 +1,5 @@
 #include "consumer.h"
+#include <iostream>
 #include <string>
 
 Consumer::Consumer(std::vector<Producer*> &producers,
