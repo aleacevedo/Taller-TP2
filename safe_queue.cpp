ee61e5c6b3833c33bf9164e1f31345ba42370316
alejoacevedo <ale.acevedo@live.com.ar>
2019-10-05 16:54:37 -0300

    Modify safequeue to have less responsibility

--- safe_queue.cpp
+++ safe_queue.cpp
@@ -1,50 +1,64 @@
 #include "safe_queue.h"
+#include <iostream>
 #include <string>
 
 SafeQueue::SafeQueue(size_t queue_limit) : my_queue(),
-                                                queue_limit(queue_limit),
-                                                lock_work_done(),
-                                                lock(),
-                                                notify_push(),
-                                                notify_pop(),
-                                                work_done(false) {
-                                                }
+                                              queue_limit(queue_limit),
+                                              lock_queue(),
+                                              lock(),
+                                              notify_push(),
+                                              notify_pop(),
+                                              work_done(false) {}
 
 void SafeQueue::push(std::string &value) {
   std::unique_lock<std::mutex> lock_push(this->lock);
-  while (this->my_queue.size() >= this->queue_limit) {
+  while (this->get_size() >= this->queue_limit) {
     this->notify_pop.wait(lock_push);
   }
-  this->my_queue.push(value);
+  this->safe_push(value);
   this->notify_push.notify_all();
 }
 
 std::string SafeQueue::pop() {
   std::unique_lock<std::mutex> lock_pop(this->lock);
-  while (this->my_queue.empty()) {
-    if (this->work_done) {
+  while (this->is_empty()) {
+    if (work_done) {
       return "";
     }
     this->notify_push.wait(lock_pop);
   }
-  std::string value = this->my_queue.front();
-  this->my_queue.pop();
-  this->notify_pop.notify_one();
+  std::string value = this->safe_pop();
+  this->notify_pop.notify_all();
   return value;
 }
 
-void SafeQueue::set_work_done() {
-  this->lock_work_done.lock();
+void SafeQueue::notify_work_done(){
   this->work_done = true;
-  this->lock_work_done.unlock();
-  this->notify_push.notify_one();
+  this->notify_pop.notify_all();
+  this->notify_push.notify_all();
 }
 
-bool SafeQueue::get_work_done() {
-  this->lock_work_done.lock();
-  bool result = this->work_done && this->my_queue.empty();
-  this->lock_work_done.unlock();
-  return result;
+size_t SafeQueue::get_size() {
+  std::lock_guard<std::mutex> lock(this->lock_queue);
+  return this->my_queue.size();
+}
+
+bool SafeQueue::is_empty() {
+  std::lock_guard<std::mutex> lock(this->lock_queue);
+  return this->my_queue.empty();
 }
 
 SafeQueue::~SafeQueue() {}
+
+
+void SafeQueue::safe_push(std::string &value) {
+  std::lock_guard<std::mutex> lock_guard(this->lock_queue);
+  this->my_queue.push(value);
+}
+
+std::string SafeQueue::safe_pop() {
+  std::lock_guard<std::mutex> lock_guard(this->lock_queue);
+  std::string value = this->my_queue.front();
+  this->my_queue.pop();
+  return value;
+}
--- safe_queue.h
+++ safe_queue.h
@@ -5,23 +5,29 @@
 #include <string>
 #include <condition_variable>
 #include <mutex>
+#include <atomic>
 
 class SafeQueue {
   std::queue<std::string> my_queue;
   size_t queue_limit;
-  std::mutex lock_work_done;
+  std::mutex lock_queue;
   std::mutex lock;
   std::condition_variable notify_push;
   std::condition_variable notify_pop;
-  bool work_done;
+  std::atomic<bool> work_done;
  public:
   SafeQueue(size_t queue_limit);
   void push(std::string &value);
   size_t size();
   std::string pop();
-  void set_work_done();
-  bool get_work_done();
+  bool is_empty();
+  size_t get_size();
+  void notify_work_done();
   ~SafeQueue();
+ private:
+  void safe_push(std::string &value);
+  std::string safe_pop();
+
 };
 
 #endif  //  SAFE_QUEUE_H_
